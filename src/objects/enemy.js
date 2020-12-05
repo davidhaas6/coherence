4c52859b9d7020cb422391a01c84ca597cbb5b75
davidhaas6 <davidhaas6@gmail.com>
2020-12-05 18:16:47 -0500

    added states to enenmy

--- src/objects/enemy.js
+++ src/objects/enemy.js
@@ -2,6 +2,49 @@
     David Haas
     Functions for the enemy turrets
 */
+//TODO: basic state machine
+
+const EnemyState = {
+    watch: 0,
+    attack: 1,
+}
+
+class AttackState {
+    constructor(self) {
+        this.self = self;
+    }
+
+    process() {
+        let self = this.self;
+
+        let toPlayer = game.activeTimeline().centroid().sub(this.self.centroid());
+        if (toPlayer.mag() > self.attackDist) {
+            self.setState(EnemyState.watch);
+        }
+
+        // attack
+        if ((secs() - self.lastAttack) > self.attackCooldown) {
+            let vel = toPlayer.setMag(7);
+            let c = self.centroid();
+            game.spawnBullet(c.x, c.y, vel, false);
+            self.lastAttack = secs();
+        }
+    }
+}
+
+class WatchState {
+    constructor(self) {
+        this.self = self;
+    }
+
+    process() {
+        let self = this.self;
+        let toPlayer = game.activeTimeline().centroid().sub(self.centroid());
+        if (toPlayer.mag() < self.attackDist) {
+            self.setState(EnemyState.attack);
+        }
+    }
+}
 
 class Enemy {
     constructor(x, y) {
@@ -9,6 +52,21 @@ class Enemy {
         this.w = 32;
         this.h = 32;
 
+        var self = this;
+
+        // state
+        this.states = [
+            new WatchState(self),
+            new AttackState(self),
+        ]
+        this.curState = EnemyState.watch;
+
+        // attacking
+        this.attackCooldown = 1;
+        this.lastAttack = -1;
+        this.attackDist = 300;
+
+        // visuals
         this.body = new Charge(this.w / 2, this.h / 2, null, false, false, 0, 16, 100000)
         this.body.color = palette.sentry;
         this.body.numTriangles = 20;
@@ -17,28 +75,23 @@ class Enemy {
         this.arm = new Charge(x, y, null, false, false, 0, 6, 100000);
         this.arm.color = palette.sentryPointer;
         this.arm.numTriangles = 8;
-
-        this.attackCooldown = 1;
-        this.lastAttack = -1;
-        this.attackDist = 300;
     }
 
     notify(event, data) {
 
     }
 
+    
+    setState(stateIdx) {
+        this.curState = stateIdx;
+    }
+
     process() {
         // hover
-        this.pos.y += sin(frameCount / 15) / 10;
+        this.pos.y += sin(frameCount / 15) / 8;
 
         // attack
-        let toPlayer = game.activeTimeline().centroid().sub(this.centroid());
-        if (toPlayer.mag() < this.attackDist && (secs() - this.lastAttack) > this.attackCooldown) {
-            let vel = toPlayer.setMag(7);
-            let c = this.centroid();
-            game.spawnBullet(c.x, c.y, vel, false);
-            this.lastAttack = secs();
-        }
+        this.states[this.curState].process();
     }
 
     centroid() {
